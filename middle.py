2128af1cbbbc65dc5fe8c3ee1df6804dee9be318
David J Kim <jhukdk@gmail.com>
2025-11-29 17:29:03 -0500

    middle_commit1

---
+++ middle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""middle.py - simple Hello World boilerplate."""
+
+def main() -> None:
+    """Entry point."""
+    print("Hello, middle!")
+
+if __name__ == "__main__":
+    main()
