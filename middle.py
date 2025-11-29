18e050fc037b378518953886c926043ac734072f
David J Kim <jhukdk@gmail.com>
2025-11-29 17:39:08 -0500

    middle2

--- middle.py
+++ middle.py
@@ -5,5 +5,10 @@ def main() -> None:
     """Entry point."""
     print("Hello, middle!")
 
+
+def otherchange() -> None:
+    """Another function to demonstrate changes."""
+    print("This is another change in middle.py")
+
 if __name__ == "__main__":
     main()
